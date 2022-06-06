6ddacccd3ec9d028ee262d0c0eff8c0c00086f24
Jeena Ks1 <jeenaks1@in.ibm.com>
2022-06-06 12:47:52 +0530

    add

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Employee {
+
+	int eno;
+	String empName;
+	double salary;
+	
+	
+	public Employee() {
+		
+	}
+	
+	public Employee(int eno, String empName, double salary) {
+		super();
+		this.eno = eno;
+		this.empName = empName;
+		this.salary = salary;
+	}
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	
+}
